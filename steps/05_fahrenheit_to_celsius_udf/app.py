d1d21e392effe0dcfbb85f22790e9d2d5746a2f0
Nicholas Davey - AAG <Nicholas.Davey@bain.com>
2023-08-16 15:20:49 +1000

    Tutorial complete

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -19,6 +19,7 @@ def main(temp_f: float) -> float:
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
+# Type conversion is where you put a column and a type after an argument name
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         print(main(*sys.argv[1:]))  # type: ignore
